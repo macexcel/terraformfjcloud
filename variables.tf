bff1c658140cd98a918021465fc385e4b0b1a7ca
Fallar <s.macexcel@jp.fujitsu.com>
2022-06-29 15:06:36 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable nifcloud_access_key_id {
+    type = string
+    default = ""
+}
+variable nifcloud_secret_access_key {
+    type = string
+    default = ""
+}
+
+variable nifcloud_region {
+    type = string
+    default = "jp-east-1"
+}
+
+variable availability_zone {
+    type = string
+    default = "east-11"
+}
